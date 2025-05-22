7c1efaa3bacd15a28cb30b774887e9dc94529d3c
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-05-22 15:30:29 +0700

    Write a program that takes two numbers as inputs and displays their sum.

--- JS1_1.test.js
+++ JS1_1.test.js
@@ -1,6 +1,19 @@
-// sum.test.js
-const sum = require('./JS1_1');
+const sum = require('./JS1_1.js');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('sum()', () => {
+  test('1 + 2 = 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test('0 + 0 = 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test('-5 + 5 = 0', () => {
+    expect(sum(-5, 5)).toBe(0);
+  });
+
+  test('1.5 + 2.5 = 4', () => {
+    expect(sum(1.5, 2.5)).toBe(4);
+  });
 });
