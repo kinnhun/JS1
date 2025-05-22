de0119eb7cc29fa6cab343f8476107bb4aaa1f1b
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-05-22 16:04:31 +0700

    Write a program that takes two strings as input and returns the longest common subsequence of the two strings. 		VD: đầu vào [“abcdef", “abczyzcdef”], Đầu ra: “cdef"

---
+++ JS2_3.test.js
@@ -0,0 +1,23 @@
+const findLongestCommonSubstring = require('./JS2_3');
+
+describe('findLongestCommonSubstring()', () => {
+  test('["abcdef", "abczyzcdef"] → "cdef"', () => {
+    expect(findLongestCommonSubstring('abcdef', 'abczyzcdef')).toBe('cdef');
+  });
+
+  test('["abc", "abc"] → "abc"', () => {
+    expect(findLongestCommonSubstring('abc', 'abc')).toBe('abc');
+  });
+
+  test('["abc", "def"] → ""', () => {
+    expect(findLongestCommonSubstring('abc', 'def')).toBe('');
+  });
+
+  test('["ababc", "babca"] → "babc"', () => {
+    expect(findLongestCommonSubstring('ababc', 'babca')).toBe('babc');
+  });
+
+  test('["", "xyz"] → ""', () => {
+    expect(findLongestCommonSubstring('', 'xyz')).toBe('');
+  });
+});
