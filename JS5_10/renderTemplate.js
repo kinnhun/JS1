3aad21b0958d694980fe34b59e7c96aa9c567345
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-05-23 09:06:04 +0700

    TemplateString: Đầu vào: một file template, và các params, nội dung file và tên các param không cố định Đầu ra: một file mới với nội dung là template và các params được truyền vào.

---
+++ JS5_10/renderTemplate.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const templateString = require('./templateString');
+
+// Bước 1: Đọc template từ file
+const template = fs.readFileSync('template.html', 'utf-8');
+
+// Bước 2: Truyền vào các params
+const params = {
+  title: 'Search of skill',
+  pageTitle: 'Home page',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, consectetur.'
+};
+
+// Bước 3: Thay thế nội dung
+const result = templateString(template, params);
+
+// Bước 4: Ghi ra file mới
+fs.writeFileSync('output.html', result, 'utf-8');
+
+console.log('✅ File HTML đã tạo: output.html');
---
+++ JS5_10/templateString.js
@@ -0,0 +1,10 @@
+function templateString(template, params) {
+  let result = template;
+  Object.keys(params).forEach(key => {
+    const regex = new RegExp(`{{\\s*${key}\\s*}}`, 'g');
+    result = result.replace(regex, params[key]);
+  });
+  return result;
+}
+
+module.exports = templateString;
